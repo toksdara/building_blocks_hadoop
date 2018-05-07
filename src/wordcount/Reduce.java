41db9b23fe01594d865118ed40644320e8868759
toksdara <Toks@Toks.connectify>
2018-05-07 19:03:36 +0100

    Initial commit

---
+++ src/wordcount/Reduce.java
@@ -0,0 +1,29 @@
+package wordcount;
+
+/**
+ * Created by Toks on 21/12/2016.
+ */
+
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class Reduce extends Reducer <Text, IntWritable, Text, LongWritable> {
+
+    @Override
+    public void reduce(Text key,
+                       Iterable<IntWritable> values,
+                       Context context)
+            throws IOException, InterruptedException {
+        long count = 0;
+        for (IntWritable value : values) {
+            count++;
+        }
+
+        context.write(key, new LongWritable(count));
+    }
+}
